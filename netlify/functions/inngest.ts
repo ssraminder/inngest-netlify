96c4e1b52445ba5de0264ae14ade49d8529603ef
ssraminder <ss.raminder@gmail.com>
2025-09-18 23:11:38 -0600

    Update inngest.ts

--- netlify/functions/inngest.ts
+++ netlify/functions/inngest.ts
@@ -1,9 +1,8 @@
 // netlify/functions/inngest.ts
-import { serve } from "@inngest/netlify";
-import { inngest } from "../../lib/inngest/client";
-import { functions } from "../../inngest/workflows";
+// import { serve } from "@inngest/netlify";
+// import { inngest } from "../../lib/inngest/client";
+// import { functions } from "../../inngest/workflows";
 
-export const handler = serve({
-  client: inngest,
-  functions,
-});
+// export const handler = serve({
+// client: inngest,// functions,
+// });
