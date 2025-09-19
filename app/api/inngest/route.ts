f068be5e9b39b856e3d04b3b32b6ab3c871363c3
ssraminder <ss.raminder@gmail.com>
2025-09-18 23:31:14 -0600

    Update route.ts

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,7 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/lib/inngest/client";
-import { functions } from "@/inngest/workflows";
+export const runtime = "nodejs";
 
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions,
-});
+import { serve } from "inngest/next";
+import { inngest } from "../../../lib/inngest/client";
+import { functions } from "../../../inngest/workflows";
+
+export const { GET, POST, PUT } = serve({ client: inngest, functions });
