e7d08b6c516ef5788fb6bb3b2fc07258c95e4f76
ssraminder <ss.raminder@gmail.com>
2025-09-18 22:13:26 -0600

    Create inngest.ts

---
+++ netlify/functions/inngest.ts
@@ -0,0 +1,9 @@
+// netlify/functions/inngest.ts
+import { serve } from "@inngest/netlify";
+import { inngest } from "../../lib/inngest/client";
+import { functions } from "../../inngest/workflows";
+
+export const handler = serve({
+  client: inngest,
+  functions,
+});
