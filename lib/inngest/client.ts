405cb25adb1f8157d51a294ade8961b82bcbf906
ssraminder <ss.raminder@gmail.com>
2025-09-18 23:49:48 -0600

    Update client.ts

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,6 +1,9 @@
-// lib/inngest/client.ts
 import { Inngest } from "inngest";
 
+// Option A: give your app a stable ID (recommended)
 export const inngest = new Inngest({
-  name: "Cethos Quote Platform",
+  id: "cethos-quote-platform",
 });
+
+// Option B: if you don't care about naming, you can also do:
+// export const inngest = new Inngest();
