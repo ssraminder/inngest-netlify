39acb4addfefc715399a625704d59d9e0f3b1196
ssraminder <ss.raminder@gmail.com>
2025-09-20 17:27:26 -0600

    Update client.ts

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,5 +1,5 @@
 //  lib/inngest/client.ts
-import { Inngest } from "inngest";
+import Inngest from "inngest";
 
 export const inngest = new Inngest({
   id: "cethos-quote-platform",      // stable, lowercase, no spaces
