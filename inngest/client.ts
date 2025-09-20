8c9d05c616008bbf34ffff86a64254a9186ff366
ssraminder <ss.raminder@gmail.com>
2025-09-20 16:42:11 -0600

    Create client.ts

---
+++ inngest/client.ts
@@ -0,0 +1,4 @@
+// inngest/client.ts
+// Compatibility shim: re-export the real client implementation from lib/inngest/client
+// so imports using "@/inngest/client" will work.
+export * from "@/lib/inngest/client";
