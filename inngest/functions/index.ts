996cb0933b697d43df9b0c161f76dcf1b34cee32
ssraminder <ss.raminder@gmail.com>
2025-09-20 16:02:47 -0600

    Update index.ts

--- inngest/functions/index.ts
+++ inngest/functions/index.ts
@@ -1,6 +1,5 @@
 // inngest/functions/index.ts
-// Re-export the named functions so imports like "@/inngest/functions" resolve cleanly.
-
 export { echoFilesUploaded } from "./echoFilesUploaded";
 export { processUpload } from "./processUpload";
 export { quoteCreatedPrepareJobs } from "./quoteCreatedPrepareJobs";
+// add other exports as you have other files
