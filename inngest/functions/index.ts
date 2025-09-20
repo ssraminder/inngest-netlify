810a92becf1942260d70b94de035037d5757fc52
ssraminder <ss.raminder@gmail.com>
2025-09-20 13:57:06 -0600

    Create index.ts

---
+++ inngest/functions/index.ts
@@ -0,0 +1,6 @@
+// inngest/functions/index.ts
+// Re-export the named functions so imports like "@/inngest/functions" resolve cleanly.
+
+export { echoFilesUploaded } from "./echoFilesUploaded";
+export { processUpload } from "./processUpload";
+export { quoteCreatedPrepareJobs } from "./quoteCreatedPrepareJobs";
