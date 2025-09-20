0a1e95860850cde8009a300b196b3f84a37d8099
ssraminder <ss.raminder@gmail.com>
2025-09-20 19:34:20 +0000

    chore: Remove conflicting Inngest workflow and function files

--- netlify/functions/inngest.ts
+++
@@ -1,8 +0,0 @@
-// netlify/functions/inngest.ts
-// import { serve } from "@inngest/netlify";
-// import { inngest } from "../../lib/inngest/client";
-// import { functions } from "../../inngest/workflows";
-
-// export const handler = serve({
-// client: inngest,// functions,
-// });
