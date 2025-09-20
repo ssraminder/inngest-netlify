81422ae0974533578b848ee595d703db79b8f569
ssraminder <ss.raminder@gmail.com>
2025-09-20 17:21:27 -0600

    Create gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,8 @@
+// gatsby-node.js
+// Minimal placeholder so Netlify/Gatsby hooks that try to require this file won't fail.
+// If you later confirm Gatsby is not used at all, you can remove this file and
+// remove the plugin/config that requires Gatsby.
+
+exports.createPages = async () => {
+  // intentionally empty
+};
