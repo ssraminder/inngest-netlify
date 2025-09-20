917ea109b5dc6fdd47369d07246eb86a27b6bbaf
ssraminder <ss.raminder@gmail.com>
2025-09-20 16:51:33 -0600

    Update route.ts

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,18 +1,34 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 
-// If you have an index exporting all functions:
+// Functions exported from the small functions folder
 import {
   echoFilesUploaded,
   processUpload,
-  quoteCreatedPrepareJobs,
+  quoteCreatedPrepareJobs as functionsQuoteCreatedPrepareJobs,
 } from "@/inngest/functions";
+
+// Functions defined in the workflows file (these were previously not imported / registered)
+import {
+  ocrDocument,
+  geminiAnalyze,
+  computePricing,
+  quoteCreatedPrepareJobs as workflowsQuoteCreatedPrepareJobs,
+} from "@/inngest/workflows";
+
+// Build a single list of functions to register. Avoid duplicate entries.
+const registeredFunctions = [
+  echoFilesUploaded,
+  processUpload,
+  // prefer the functions/ version if present, otherwise the workflows version
+  functionsQuoteCreatedPrepareJobs ?? workflowsQuoteCreatedPrepareJobs,
+  // add the workflow functions that were missing previously:
+  ocrDocument,
+  geminiAnalyze,
+  computePricing,
+].filter(Boolean);
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    echoFilesUploaded,
-    processUpload,
-    quoteCreatedPrepareJobs,
-  ],
+  functions: registeredFunctions,
 });
