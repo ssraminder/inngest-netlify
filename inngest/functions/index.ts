1d8386609470be3386c034fa01d35ae4b67d8b4d
ssraminder <ss.raminder@gmail.com>
2025-09-23 21:38:51 +0000

    chore(inngest): stop exporting duplicate quoteCreatedPrepareJobs from inngest/functions

--- inngest/functions/index.ts
+++ inngest/functions/index.ts
@@ -1,5 +1,4 @@
 // inngest/functions/index.ts
 export { echoFilesUploaded } from "./echoFilesUploaded";
 export { processUpload } from "./processUpload";
-export { quoteCreatedPrepareJobs } from "./quoteCreatedPrepareJobs";
 // add other exports as you have other files
